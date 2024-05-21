e37b82751d2e9e8301f29d41828b1bb7a2d2808b
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-21 21:46:05 +1000

    maven

---
+++ run.sh
@@ -0,0 +1,5 @@
+mvn clean install
+echo
+echo Done building
+echo
+java -jar target/Server-1.0.jar
--- src/cf/pies/server/utils/ActionUtil.java
+++ src/cf/pies/server/utils/ActionUtil.java
@@ -2,7 +2,6 @@ package cf.pies.server.utils;
 
 import cf.pies.server.Main;
 import cf.pies.server.server.Instance;
-import com.sun.istack.internal.Nullable;
 
 public class ActionUtil {
     private static final Main main = Main.get();
@@ -12,7 +11,7 @@ public class ActionUtil {
      * @param stringId The string id of the instance
      * @return The instance if it exists, or null if it does not exist or there is an error
      */
-    public static @Nullable Instance getInstance(String stringId) {
+    public static Instance getInstance(String stringId) {
         int id;
         try {
             id = Integer.parseInt(stringId);
