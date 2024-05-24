f3c0bbf66844dd526855bc8089ddbec602a508a2
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-24 16:27:13 +1000

    quotes

--- src/cf/pies/server/ConfigLoader.java
+++ src/cf/pies/server/ConfigLoader.java
@@ -9,10 +9,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConfigLoader {
     public static void loadConfig(Main main) {
@@ -42,9 +41,7 @@ public class ConfigLoader {
                     Logger.log("Instance " + name + " already exists.");
                     continue;
                 }
-                List<String> commands = Arrays.asList(
-                        serverObject.get("command").getAsString().split(" ")
-                );
+                List<String> commands = splitSpace(serverObject.get("command").getAsString());
                 Instance instance = new Instance(name, commands);
                 if (serverObject.has("path")) {
                     instance.path(serverObject.get("path").getAsString());
@@ -60,5 +57,25 @@ public class ConfigLoader {
             Logger.error(e);
             Logger.log("Failed to read config.");
         }
+    }
+
+    public static List<String> splitSpace(String str) {
+        List<String> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\"([^\"]*)\"|\\S+");
+        Matcher matcher = pattern.matcher(str);
+
+        // Print the parts
+        while (matcher.find()) {
+            String match = matcher.group();
+            if (match.startsWith("\"")) {
+                match = match.substring(1);
+            }
+            if (match.endsWith("\"")) {
+                match = match.substring(0, match.length() - 1);
+            }
+            list.add(match);
+        }
+
+        return list;
     }
 }
